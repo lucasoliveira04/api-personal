d5918e46f36d0886ecd9ad57d7f7998e0119253f
= <lucasolisocialmedia@gmail.com>
2025-07-12 12:45:22 -0800

    update: checking connection with supabase before initializing application

--- src/app.py
+++ src/app.py
@@ -1,6 +1,6 @@
 from flask import Flask
 from controllers.profile_controller import profile_controller
-
+from services.supabase import get_connection
 
 app = Flask(__name__)
 
@@ -9,4 +9,8 @@ PREFIX = "/api/v1"
 app.register_blueprint(profile_controller, url_prefix=PREFIX)
 
 if __name__ == "__main__":
-    app.run(debug=True, host='0.0.0.0', port=5000)
+    if get_connection():
+        app.run(debug=True, host='0.0.0.0', port=5000)
+    else:
+        print("Failed to connect to the database. Please check your connection settings.")
+        exit(1)
